6f3d9578b06d7f34d25373447d462975174ff74a
nagbabalik <104643052+nagbabalik@users.noreply.github.com>
2022-05-05 18:52:44 -0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","login");
+$con = mysqli_connect("fdb18.atspace.me","3354968_mjt","hulaanmo5878","3354968_mjt");
 
 // Check connection
 if (mysqli_connect_errno())
