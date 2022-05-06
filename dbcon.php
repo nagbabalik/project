181432f042276c8ebc8bf2d75e36a6abe9650cc9
nagbabalik <104643052+nagbabalik@users.noreply.github.com>
2022-05-05 21:39:36 -0800

    Delete dbcon.php

--- dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect("fdb18.atspace.me","3354968_mjt","hulaanmo5878","3354968_mjt");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
